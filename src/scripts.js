1bc3818938d781563b099e2087fd11aaafc9d8c9
Ashish <yesashishs@gmail.com>
2020-07-17 18:39:10 -0400

    API work so far

--- src/scripts.js
+++ src/scripts.js
@@ -1,26 +1,29 @@
 //Search for nearby GeoFences
 //If none found create a GeoFence
-window.addEventListener("load",setup);
 
 async function setup(){
     Radar.initialize('prj_live_pk_20d3fdd8f9d6c1dc196b415ac11a9686b0e36be4');
-    let latlong;
-    console.log(navigator.geolocation.getCurrentPosition(function(pos){
-      console.log('Success');
-      console.log(pos);
-      latlong=[$pos.coords.latitude,pos.coords.longitude]
-    }), );
-    
-    const location = { near: $latlong} //TODO: Add latlong of user
-    let result = await findGeofence(location);
-    if(result===0){
-      const data = {
-        description: SomeName,
-        type: circle,
-        coordinates: $latlong, //TODO: Add latlong of user
-        radius: 1500, //1.5km around ship
+    let latlong=[];
+    await Radar.getLocation(function(err, result) {
+      if (!err) {
+        latlong = [result.location.latitude,result.location.longitude];
+        console.log(result);
+        const location = { near: latlong};
       }
-      await createGeofence(data);
+    });
+    if(latlong.length>0){
+      console.log(latlong);
+       //TODO: Add latlong of user
+      let result = await findGeofence(location);
+      if(result===0){
+        const data = {
+          description: SomeName,
+          type: circle,
+          coordinates: $latlong, //TODO: Add latlong of user
+          radius: 1500, //1.5km around ship
+        }
+        await createGeofence(data);
+      }
     }
     //setInterval(draw,1000);
 }
@@ -39,6 +42,7 @@ async function createGeofence(data){
 }
 
 async function findGeofence(location){
+  console.log('Find Geofence');
   let response = await fetch('https://api.radar.io/v1/search/geofences', {
       method: 'GET',
       headers: {
