fe4198e1469d0162db4e4a027d41f83d8ac30d0b
Adit-COCO-Garg <adit.garg21k@gmail.com>
2020-07-17 17:12:16 -0400

    local changes

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 window.addEventListener("load",setup);
 
-function setup(){
+function setup() {
     navigator.geolocation;
+    Radar.initialize('prj_live_pk_20d3fdd8f9d6c1dc196b415ac11a9686b0e36be4');
     setInterval(draw,1000);
 }
 
 
-function draw(){
-    // Radar.initialize('prj_live_pk_20d3fdd8f9d6c1dc196b415ac11a9686b0e36be4');
+function draw() {
     Radar.getLocation((err,result)=>{
         if (err) console.error(err);
-        else console.log(result)
+        else console.log(result);
     });
 }
