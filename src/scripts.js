b490c1299df87855193e50d6681b6f8eb713d6e0
Ashish <yesashishs@gmail.com>
2020-07-17 21:41:06 -0400

    The website will now attempt to create a geofence if it doesnt find any nearby

--- src/scripts.js
+++ src/scripts.js
@@ -4,19 +4,17 @@
 async function setup(){
   Radar.initialize('prj_live_pk_20d3fdd8f9d6c1dc196b415ac11a9686b0e36be4');
   let latlong=[];
-  await Radar.getLocation(function(err, result) {
+  await Radar.getLocation(async function(err, result) {
     if (!err) {
-      latlong = [result.location.latitude,result.location.longitude];
+      latlong = [result.location.longitude,result.location.latitude];
       console.log(result);
       if(latlong.length>0){
         console.log(latlong);
-        findGeofence(latlong).then(result=>{
-          console.log("RESULT: "+result);
-          if(result==0){
-            
-            createGeofence(someName, circle, latlong, 1500);
-          }
-        });
+        let result = await findGeofence(latlong);
+        console.log("RESULT: "+result);
+        if(result==0){
+          createGeofence("someName", "circle", latlong, 10000);
+        }
       }
     }
   });
@@ -24,12 +22,12 @@ async function setup(){
 
 async function createGeofence(store,type,coordinates){
 console.log('Create Geofence');
+console.log(`https://api.radar.io/v1/geofences?description=${store}&type=${type}&coordinates=${coordinates}&radius=1500`);
 let response = await fetch(`https://api.radar.io/v1/geofences?description=${store}&type=${type}&coordinates=${coordinates}&radius=1500`, {
     method: 'POST',
     headers: {
     "Authorization": "prj_live_pk_20d3fdd8f9d6c1dc196b415ac11a9686b0e36be4"
     },
-    body: JSON.stringify(data)
 }); 
 if(response.ok){
     console.log(await response.json());
@@ -38,6 +36,7 @@ if(response.ok){
 
 async function findGeofence(location){
   console.log('Find Geofence');
+  let returnValue = 0;
   let response = await fetch(`https://api.radar.io/v1/search/geofences?near=${location}`, {
       method: 'GET',
       headers: {
@@ -48,15 +47,17 @@ async function findGeofence(location){
   .then(data =>{
     console.log(data);
     console.log(data.geofences);
+    console.log(data.geofences.length==0);
     if(data.geofences.length==0){
-      return 0;
+      returnValue=0;
     }
     else{
-      return 1;
+      returnValue=1;
     }
   })
   .catch(e=>{
-    return 0;
+    returnValue=0;
   })
   ); 
+  return returnValue;
 }
